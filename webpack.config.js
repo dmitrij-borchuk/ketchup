5140e591e9b71259214620bd6f26623f7e326d44
Dmytro Borchuk <frunk.lern@gmail.com>
2018-03-14 22:34:05 +0200

    Add git hooks (#4)
    * Added Git hooks
    
    * Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,24 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(webmanifest)$/i,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      },
     ],
   },
 };
